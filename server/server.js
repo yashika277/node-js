d9ac2c3313fcdda6f460b7aadcddb8cec8b26b8f
yashika277 <yashika.bhadkoliya2707@gmail.com>
2024-05-22 21:28:38 +0530

    create-server

---
+++ server/server.js
@@ -0,0 +1,122 @@
+const { log } = require("console")
+let http = require("http")
+
+http.createServer((req, res) => {
+    res.end("yashika1")
+}).listen(3001, () => {
+    console.log("server is running port 3001");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika2")
+}).listen(3002, () => {
+    console.log("server is running port 3002");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika3")
+}).listen(3003, () => {
+    console.log("server is running port 3003");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika4")
+}).listen(3004, () => {
+    console.log("server is running port 3004");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika5")
+}).listen(3005, () => {
+    console.log("server is running port 3002");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika6")
+}).listen(3006, () => {
+    console.log("server is running port 3006");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika7")
+}).listen(3007, () => {
+    console.log("server is running port 3007");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika8")
+}).listen(3008, () => {
+    console.log("server is running port 3008");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika9")
+}).listen(3009, () => {
+    console.log("server is running port 3009");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika10")
+}).listen(3010, () => {
+    console.log("server is running port 3010");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika11")
+}).listen(3011, () => {
+    console.log("server is running port 3011");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika12")
+}).listen(3012, () => {
+    console.log("server is running port 3012");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika13")
+}).listen(3013, () => {
+    console.log("server is running port 3013");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika14")
+}).listen(3014, () => {
+    console.log("server is running port 3014");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika15")
+}).listen(3015, () => {
+    console.log("server is running port 3015");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika16")
+}).listen(3016, () => {
+    console.log("server is running port 3016");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika17")
+}).listen(3017, () => {
+    console.log("server is running port 3017");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika18")
+}).listen(3018, () => {
+    console.log("server is running port 3018");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika19")
+}).listen(3019, () => {
+    console.log("server is running port 3019");
+})
+
+http.createServer((req, res) => {
+    res.end("yashika20")
+}).listen(3020, () => {
+    console.log("server is running port 3020");
+})
---
+++ server/test.js
@@ -0,0 +1,3 @@
+let name = "yashika";
+
+module.exports = name;
